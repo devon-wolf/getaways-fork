9ef3fc435c98e6e4d98c9ccfdfcaebaeefab9290
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-25 11:19:27 -0700

    Remove get state function because that was NOT the state the backend was talking about...

--- bookings-web/src/containers/BookingPage.jsx
+++ bookings-web/src/containers/BookingPage.jsx
@@ -17,22 +17,15 @@ const BookingPage = ({ match }) => {
 		return nights * place.pricePerNight;
 	}
 
-	const getState = () => {
-		const splitLocation = place.location.split(',');
-		return splitLocation[splitLocation.length - 2];
-	}
-
 	const submitBooking = e => {
 		e.preventDefault();
 		console.log('Start date:', startDate, 'End date:', endDate, 'Price:', calculatePrice(), 'Location:', getState());
 
 		const booking = {
-			start_date: new Date(startDate),
-			end_date: new Date(endDate),
-			state: getState(),
-			total_price: calculatePrice(),
+			start_date: startDate,
+			end_date: endDate,
 			place_id: place.id,
-			user_id: match.params.user.id
+			// user_id: match.params.user.id
 		};
 
 		// send booking to backend
